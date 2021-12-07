a9323fb5f97bfc535828ffb4191c19f0ac1d37c2
TTMead <timothy.mead20@gmail.com>
2021-12-07 18:51:44 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2 # Video Capturing / Image Processing
+import numpy as np
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # Begin Video Capturing
+
+# Check for successful video access
+if not (cap.isOpened()):
+    print("Could not open video device")
+    exit(1)  # Exit the program with error code
+
+
+# Main Loop
+while True:
+    # Read a new image from video capture
+    ret, frame = cap.read()
+
+    # resizing for faster detection
+    frame = cv2.resize(frame, (640, 480))
+    # using a greyscale picture, also for faster detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+
+    # detect people in the image
+    # returns the bounding boxes for the detected objects
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+    for (xA, yA, xB, yB) in boxes:
+        # display the detected boxes in the colour picture
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
+    
+    
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
