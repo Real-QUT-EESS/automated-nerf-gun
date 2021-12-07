9b9763cf77b1a59dc64654e169df24ce438f981d
BuildTools <unconfigured@null.spigotmc.org>
2021-12-07 19:58:30 +1000

    Added face detection option, as the hog file does not accurately detect people if the whole body is shown. Could combine in future.

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import numpy as np # Array manipulation
 
 from person_locator import * # Person detection
 
+UseFacialDetection = 1
+
+# Put the video capturing in a seperate thread
+cv2.startWindowThread()
 
 # Begin Video Capturing
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -26,19 +30,26 @@ while True:
     # using a greyscale picture, also for faster detection
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    # Use the hog model to detect people in the frames
-    (boxes, weights) = DetectPeople(frame, model)
+    if UseFacialDetection:
+        for location in DetectFace(gray):
+            # Draw a rectangle around this face
+            cv2.rectangle(frame, (location[3], location[0]), (location[1], location[2]), (0, 0, 255), 4)
+    else:
+        # Use the hog model to detect people in the frames
+        boxes = DetectPeople(gray, model)
 
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+        boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
 
-    for (xA, yA, xB, yB) in boxes:
-        # display the detected boxes in the colour picture
-        cv2.rectangle(frame, (xA, yA), (xB, yB),
-                          (0, 255, 0), 2)
+        for (xA, yA, xB, yB) in boxes:
+            # display the detected boxes in the colour picture
+            cv2.rectangle(frame, (xA, yA), (xB, yB),
+                            (0, 255, 0), 2)
     
     
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
+
+    # Quit if q is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
--- person_locator.py
+++ person_locator.py
@@ -1,4 +1,5 @@
 import cv2
+import face_recognition
 
 # Initializes the HOG descriptor/person detector
 def CreateHog():
@@ -6,9 +7,12 @@ def CreateHog():
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     return hog
 
-
 def DetectPeople(frame, hog):
     # detect people in the image
     # returns the bounding boxes for the detected objects
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
-    return (boxes, weights)
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8), scale=1.05)
+    return boxes
+
+
+def DetectFace(frame):
+    return face_recognition.face_locations(frame)
